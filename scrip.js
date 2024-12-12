78653f172083412b0c7aacfa037ac3e08922bae4
LORD111051 <nurboyevamunira44@gmail.com>
2024-12-12 17:52:10 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,11 @@
+// console.log(prompt`Ismingizni kiriting`)
+
+// console.log(prompt`Familiangizni kiriting`)
+
+// console.log(prompt`Yoshingizni kiriting`)
+
+// console.log(prompt`Yoqtirgan faningizni kiriting`)
+
+// console.log(prompt`Yashaydigan tumaningizni kiriting`)
+
+// alert(`Malumotlaringizni kiritkaningiz uchun tashakur`)
